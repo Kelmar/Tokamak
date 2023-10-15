c8ee0ab100133f62fec0aa87087cb2723ddb5859
Kelmar Firesun <kfiresun@gmail.com>
2023-10-15 14:54:10 -0500

    Adding basic config - Using this to control Vulkan options.

---
+++ Tokamak/Config/IConfigReader.cs
@@ -0,0 +1,10 @@
+﻿namespace Tokamak.Config
+{
+    /// <summary>
+    /// This is a really simple config system for now.
+    /// </summary>
+    public interface IConfigReader
+    {
+        public T Get<T>(string name, T defValue = default);
+    }
+}
