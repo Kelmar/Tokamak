8401f6508f20628cc6e2deadd8f1c39b386dd27c
Kelmar Firesun <kfiresun@gmail.com>
2025-08-15 00:41:09 -0500

    Unit tests for Lerp()

---
+++ Tests/MathematicsTests/MathXTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+using Tokamak.Mathematics;
+
+namespace MathTests
+{
+    public class MathXTests
+    {
+
+        public static float[] FloatLerps = [0, 0.5f, 1];
+        public static double[] DoubleLerps = [0, 0.5, 1];
+
+        [TestCaseSource(nameof(FloatLerps))]
+        public void FloatLerp(float value)
+        {
+            float result = MathX.LerpF(0, 1, value);
+
+            Assert.That(result, Is.InRange(value - MathX.FUZ, value + MathX.FUZ));
+        }
+
+        [TestCaseSource(nameof(DoubleLerps))]
+        public void DoubleLerp(double value)
+        {
+            double result = MathX.Lerp(0, 1, value);
+
+            Assert.That(result, Is.InRange(value - MathX.FUZ, value + MathX.FUZ));
+        }
+    }
+}
