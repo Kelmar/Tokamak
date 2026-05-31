0bca75c013bb23789ba3883ddc56f6fe7ffc3e6c
Kelmar Firesun <kfiresun@gmail.com>
2026-05-31 16:56:02 -0500

    Utility work - Added additional IFactory generics. - Added ReferenceCounter class to provide consistent reference counts of assets.

--- Source/Tokamak.Utilities/IFactory.cs
+++ Source/Tokamak.Utilities/IFactory.cs
@@ -4,9 +4,25 @@
     /// Interface to a factory class.
     /// </summary>
     /// <typeparam name="T">The type the factory will create.</typeparam>
-    public interface IFactory<T>
+    public interface IFactory<out T>
         where T : class
     {
         T Build();
+    }
+
+    public interface IFactory<T, out TResult>
+        where TResult : class
+    {
+        TResult Build(T arg);
+    }
+
+    public interface IFactory<T1, T2, out TResult>
+    {
+        TResult Build(T1 arg1, T2 arg2);
+    }
+
+    public interface IFactory<T1, T2, T3, out TResult>
+    {
+        TResult Build(T1 arg1, T2 arg2, T3 arg3);
     }
 }
---
+++ Source/Tokamak.Utilities/ReferenceCounter.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading;
+
+namespace Tokamak.Utilities
+{
+    /// <summary>
+    /// Provides a thread safe reference counter that can be incremented or decremented.
+    /// </summary>
+    /// <remarks>
+    /// This is similar to a <see cref="System.Runtime.CompilerServices.StrongBox"/>&lt;int&gt; in that it
+    /// provides a boxed integer that can be reference passed and modified.  The difference is that it is
+    /// gated behind thread safe <see cref="System.Threading.Interlock"/>.Increment/Decrement calls, so that
+    /// access to the underlying value is maintained in a consistent thread safe way.
+    /// </remarks>
+    public class ReferenceCounter
+    {
+        private int m_value;
+
+        /// <summary>
+        /// Initializes the reference counter.
+        /// </summary>
+        /// <param name="value">The initial value to set the count to.  Defaults to 1</param>
+        public ReferenceCounter(int value = 1)
+        {
+            m_value = value;
+        }
+
+        public int Value => m_value;
+
+        /// <summary>
+        /// Increments the reference counter.
+        /// </summary>
+        /// <returns>The value of the reference counter immediately after it was incremented.</returns>
+        public int Increment()
+        {
+            return Interlocked.Increment(ref m_value);
+        }
+
+        /// <summary>
+        /// Decrements the reference counter.
+        /// </summary>
+        /// <returns>The value of the reference counter immediately after it was decremented.</returns>
+        public int Decrement()
+        {
+            return Interlocked.Decrement(ref m_value);
+        }
+    }
+}
